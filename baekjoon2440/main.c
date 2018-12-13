310456a6184961e8804669fcfa5cd5e3d0a08bed
Gilbert <ktf2002kzz@gmail.com>
2018-12-13 17:06:00 +0900

    initial commit

---
+++ baekjoon2440/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  baekjoon2440
+//
+//  Created by 이승섭 on 2018. 12. 13..
+//  Copyright © 2018년 이승섭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int N, i, j;
+    
+    scanf("%d", &N);
+    
+    if(N >= 1 && N <= 100)
+    {
+        for(i = N; i > 0; i--)
+        {
+            for(j = i; j > 0; j--)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
